55aa100fd73eab65bef594412ee49508d3058b47
Derrick Tan D-WEBDEV <derricktpl@derrickwebdev.com>
2021-08-28 12:32:20 +0800

    Add files via upload

---
+++ The PADS.sql
@@ -0,0 +1,10 @@
+SELECT 
+CONCAT(Name, '(',substr(Occupation,1,1),')') 
+FROM OCCUPATIONS
+ORDER BY Name ASC;
+
+SELECT
+CONCAT('There are a total of ', COUNT(Occupation),' ',lower(Occupation) ,'s.')
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(Occupation), Occupation ASC;
